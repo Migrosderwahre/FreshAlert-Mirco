1f091bba385df6dc383d402eb3b56f4ea73d4505
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-21 16:43:22 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,2 +1,2 @@
 import streamlit as st
-bild = st.image('https://github.com/Migrosderwahre/FreshAlert-Mirco/assets/162097689/2f552400-fd88-4306-b268-fb30024acd4a', caption='test')
+bild = st.image('18-04-_2024_11-16-47.png', caption='test')
