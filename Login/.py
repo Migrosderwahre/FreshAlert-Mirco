9f0c83d57953f3b2d011e75b0d3c503902dda4ab
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-21 16:59:30 +0200

    Update .py

--- Login/.py
+++ Login/.py
@@ -1,1 +1,28 @@
-
+CREATE TABLE users (
+    username text not null default ''::text,
+    password text not null,
+    constraint users_pkey primary key (username),
+    constraint users_username_key unique (username),
+    constraint users_password_check check (
+      (
+        length(
+          trim(
+            both
+            from
+              password
+          )
+        ) > 1
+      )
+    ),
+    constraint users_username_check check (
+      (
+        length(
+          trim(
+            both
+            from
+              username
+          )
+        ) > 1
+      )
+    )
+  ) tablespace pg_default;
