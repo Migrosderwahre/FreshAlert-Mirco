509a9faced03dd94805b975492b5e5cce758e282
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-21 16:52:19 +0200

    Update App.py

--- App.py
+++ App.py
@@ -3,6 +3,6 @@ import streamlit as st
 # Bild einfügen
 bild = st.image('18-04-_2024_11-16-47.png', caption='Test')
 
-bild3 = st.image('logo.md')
+bild3 = st.image('21-04-_2024_16-28-26.png')
 
 st.set_page_config(page_title="FreshAlert", page_icon="bild3", layout="wide")
