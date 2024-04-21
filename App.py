9b82baba204b15686e4b3b6aed84c18499b2c6c9
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-21 16:46:00 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,12 @@
 import streamlit as st
-bild = st.image('18-04-_2024_11-16-47.png', caption='test')
-<img src="Pfad/zum/Bild.jpg" alt="Alternativer Text" style="position: absolute; top: 0; right: 0;">
+
+# Bild einfügen
+bild = st.image('Pfad/zum/Bild.jpg', caption='Alternativer Text')
+
+# CSS-Stile für die Bildpositionierung
+bild.markdown(
+    f'<style>img.stImage {{ position: absolute; top: 0; right: 0; }}</style>',
+    unsafe_allow_html=True
+)
+
 
