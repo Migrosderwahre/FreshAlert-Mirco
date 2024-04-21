7bdfdb11ee59ff5e596b7f67e43643dc5a8249db
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-21 16:48:31 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Bild einfügen
-bild = st.image('18-04-_2024_11-16-47.png')
+bild = st.image('18-04-_2024_11-16-47.png', caption='Test')
 
 # CSS-Stile für die Bildpositionierung
 bild.markdown(
