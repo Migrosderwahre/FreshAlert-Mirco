1e121f66f1f9c62d2b1ed15053c4d5ce469ae592
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-21 17:06:26 +0200

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,3 @@ import streamlit as st
 from Login.Test import 
 # Bild einfügen
 st.sidebar.image('18-04-_2024_11-16-47.png', use_column_width=True)
-
-bild3 = st.image('21-04-_2024_16-28-26.png')
-
-st.set_page_config(page_title="FreshAlert", page_icon="bild3", layout="wide")
