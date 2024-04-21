3ddf55393067ab1e51c1606e6d51f7482cbedc38
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-21 16:44:34 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,2 +1,4 @@
 import streamlit as st
 bild = st.image('18-04-_2024_11-16-47.png', caption='test')
+<img src="Pfad/zum/Bild.jpg" alt="Alternativer Text" style="position: absolute; top: 0; right: 0;">
+
