faa3ccc8d6263fa0406a7ed7ed2f736b7488d942
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-21 16:38:58 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,24 +1,2 @@
 import streamlit as st
-from functions.database_new import *
-from functions.utilities import *
-from functions.user_functions import *
-from functions.quick_functions import *
-
-if 'loginstatus' not in st.session_state:
-    st.session_state['loginstatus'] = False
-
-if 'loggedinuser' not in st.session_state:
-    st.session_state['loggedinuser'] = False
-
-if 'loggedinuserid' not in st.session_state:
-    st.session_state['loggedinuserid'] = False
-
-if 'aktuell' not in st.session_state:
-    st.session_state['aktuell'] = 'Startseite'
-else:
-    st.session_state['aktuell'] = 'Startseite'
-
-if __name__ == '__main__':
-    main(st.session_state['loginstatus'])
-    user_database()
-    setup_quickdatabase(st.session_state['loggedinuserid'])
+bild = st.image('', caption='Bildbeschreibung')
