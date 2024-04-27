524ea05fa490331210c949109c798ac124b88fae
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-27 17:09:20 +0200

    Update App.py

--- App.py
+++ App.py
@@ -2,10 +2,6 @@ import streamlit as st
 import pandas as pd
 from github_contents import GithubContents
 
-# Set constants for user registration
-DATA_FILE = "FreshAlert-Registration.csv"
-DATA_COLUMNS = ["Vorname", "Nachname", "E-Mail", "Passwort", "Passwort wiederholen"]
-
 # Set constants for fridge contents
 DATA_FILE_FOOD = "FridgeContents.csv"
 DATA_COLUMNS_FOOD = ["Lebensmittel", "Kategorie", "Lagerort", "Ablaufdatum", "Standort"]
@@ -27,13 +23,6 @@ def init_github():
             st.secrets["github"]["token"]
         )
 
-def init_dataframe_login():
-    """Initialize or load the dataframe for user registration."""
-    if 'df' not in st.session_state:
-        if st.session_state.github.file_exists(DATA_FILE):
-            st.session_state.df_login = st.session_state.github.read_df(DATA_FILE)
-        else:
-            st.session_state.df_login = pd.DataFrame(columns=DATA_COLUMNS)
 
 def init_dataframe_food():
     """Initialize or load the dataframe for fridge contents."""
@@ -43,51 +32,7 @@ def init_dataframe_food():
         else:
             st.session_state.df_food = pd.DataFrame(columns=DATA_COLUMNS_FOOD)
 
-def show_login_page():
-    st.title("Login")
-    email = st.text_input("E-Mail", key="login_email")
-    password = st.text_input("Passwort", type="password", key="login_password")
-    if st.button("Login"):
-        login_successful = False
-        for index, row in st.session_state.df_login.iterrows():
-            if row["E-Mail"] == email and row["Passwort"] == password:
-                login_successful = True
-                break
-        if login_successful:
-            st.session_state.user_logged_in = True
-            st.success("Erfolgreich eingeloggt!")
-        else:
-            st.error("Ungültige E-Mail oder Passwort.")
-    if st.button("Registrieren", key="registration_button"):
-        st.session_state.show_registration = True
-    if st.session_state.get("show_registration", False):
-        show_registration_page()
 
-def show_registration_page():
-    st.title("Registrieren")
-           
-    new_entry = {
-        DATA_COLUMNS[0]: st.text_input(DATA_COLUMNS[0]), #Vorname
-        DATA_COLUMNS[1]: st.text_input(DATA_COLUMNS[1]), #Nachname
-        DATA_COLUMNS[2]: st.text_input(DATA_COLUMNS[2]), # E-Mail
-        DATA_COLUMNS[3]: st.text_input(DATA_COLUMNS[3], type="password"), #Passwort
-        DATA_COLUMNS[4]: st.text_input(DATA_COLUMNS[4], type="password"), #Passwort wiederholen
-    }
-
-    for key, value in new_entry.items():
-        if value == "":
-            st.error(f"Bitte ergänze das Feld '{key}'")
-            return
-
-    if st.button("Registrieren"):
-        if new_entry["Passwort"] == new_entry["Passwort wiederholen"]:
-            new_entry_df = pd.DataFrame([new_entry])
-            st.session_state.df_login = pd.concat([st.session_state.df_login, new_entry_df], ignore_index=True)
-            save_data_to_database_login()
-            st.success("Registrierung erfolgreich!")
-            st.session_state.show_registration = False  # Reset status
-        else:
-            st.error("Die Passwörter stimmen nicht überein.")
 
 def show_my_fridge_page():
     """Display the contents of the fridge."""
@@ -197,15 +142,8 @@ def save_data_to_database_food():
 
 def main():
     init_github()
-    init_dataframe_login()
     init_dataframe_food()
-    if 'user_logged_in' not in st.session_state:
-        st.session_state.user_logged_in = False
-
-    if not st.session_state.user_logged_in:
-        show_login_page()
-    else:
-        show_fresh_alert_page()
+    show_fresh_alert_page()
 
 if __name__ == "__main__":
     main()
