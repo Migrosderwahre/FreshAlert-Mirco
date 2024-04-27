9f711415a3c6d85252c7b1ed4ef6f05ed4b43677
Migrosderwahre <162097689+Migrosderwahre@users.noreply.github.com>
2024-04-27 17:13:12 +0200

    Update App.py

--- App.py
+++ App.py
@@ -33,7 +33,6 @@ def init_dataframe_food():
             st.session_state.df_food = pd.DataFrame(columns=DATA_COLUMNS_FOOD)
 
 
-
 def show_my_fridge_page():
     """Display the contents of the fridge."""
     st.title("Mein Kühlschrank")
@@ -43,53 +42,6 @@ def show_my_fridge_page():
         st.dataframe(st.session_state.df_food)
     else:
         st.write("Der Kühlschrank ist leer.")
-
-
-def show_my_fridge():
-    st.title("Lebensmittel hinzufügen")
-           
-    new_entry = {
-        DATA_COLUMNS_FOOD[0]: st.text_input(DATA_COLUMNS_FOOD[0]), #Lebensmittel
-        DATA_COLUMNS_FOOD[1]: st.text_input(DATA_COLUMNS_FOOD[1]), #Kategorie
-        DATA_COLUMNS_FOOD[2]: st.text_input(DATA_COLUMNS_FOOD[2]), # Lagerort
-        DATA_COLUMNS_FOOD[3]: st.text_input(DATA_COLUMNS_FOOD[3], type="date"), #Ablaufdatum
-        DATA_COLUMNS_FOOD[4]: st.text_input(DATA_COLUMNS_FOOD[4],), #Standort
-    }
-
-    for key, value in new_entry.items():
-        if value == "":
-            st.error(f"Bitte ergänze das Feld '{key}'")
-            return
-
-    if st.button("hinzufügen"):
-        if new_entry["Passwort"] == new_entry["Passwort wiederholen"]:
-            new_entry_df = pd.DataFrame([new_entry])
-            st.session_state.df_food = pd.concat([st.session_state.df_food, new_entry_df], ignore_index=True)
-            save_data_to_database_food()
-            st.success("Registrierung erfolgreich!")
-            st.session_state.show_my_fridge_page = False  # Reset status
-        else:
-            st.error("Die Passwörter stimmen nicht überein.")
-
-
-def show_fresh_alert_page():
-    st.title("FreshAlert")
-    st.subheader("Herzlich Willkommen bei FreshAlert. Deine App für deine Lebensmittel! "            
-"Füge links deine ersten Lebensmittel zu deinem Digitalen Kühlschrank hinzu. "
-"Wir werden dich daran erinnen, es rechtzeitig zu benutzen und dir so helfen keine Lebensmittel mehr zu verschwenden. "
-"#StopFoodwaste ")
-    st.sidebar.image('18-04-_2024_11-16-47.png', use_column_width=True)
-    st.sidebar.title("")
-    if st.sidebar.button("Mein Kühlschrank"):
-        show_my_fridge_page()
-    if st.sidebar.button("Neues Lebensmittel hinzufügen"):
-        add_food_to_fridge()
-    st.sidebar.markdown("---")  # Separator
-    if st.sidebar.button("Freunde einladen"):
-        show_my_friends()
-    if st.sidebar.button("Einstellungen"):
-        show_settings()
-
 
 
 def add_food_to_fridge():
@@ -105,11 +57,10 @@ def add_food_to_fridge():
     if st.button("Lebensmittel hinzufügen"):
         if food_name and category and location and area and expiry_date:
             new_entry_food = pd.DataFrame([[food_name, category, location, expiry_date, area]], columns=DATA_COLUMNS_FOOD)
-            new_df = pd.concat([st.session_state.df_food, new_entry_food], ignore_index=True)
-            st.session_state.df_food = new_df
+            st.session_state.df_food = pd.concat([st.session_state.df_food, new_entry_food], ignore_index=True)
             save_data_to_database_food()
             st.success("Lebensmittel erfolgreich hinzugefügt!")
-            st.write(new_df)
+            st.write(st.session_state.df_food)  # Anzeige des aktualisierten DataFrame
         else:
             st.error("Bitte füllen Sie alle Felder aus.")
 
@@ -120,30 +71,43 @@ def add_food_to_fridge():
         st.write("Der Kühlschrank ist leer.")
 
 
-
 def save_data_to_database_food():
     if 'github' in st.session_state:
         st.session_state.github.write_df(DATA_FILE_FOOD, st.session_state.df_food, "Updated food data")
+
+
+def show_fresh_alert_page():
+    st.title("FreshAlert")
+    st.subheader("Herzlich Willkommen bei FreshAlert. Deine App für deine Lebensmittel! "            
+                 "Füge links deine ersten Lebensmittel zu deinem Digitalen Kühlschrank hinzu. "
+                 "Wir werden dich daran erinneren, es rechtzeitig zu benutzen und dir so helfen, keine Lebensmittel mehr zu verschwenden. "
+                 "#StopFoodwaste ")
+    st.sidebar.image('18-04-_2024_11-16-47.png', use_column_width=True)
+    st.sidebar.title("")
+    if st.sidebar.button("Mein Kühlschrank"):
+        show_my_fridge_page()
+    if st.sidebar.button("Neues Lebensmittel hinzufügen"):
+        add_food_to_fridge()
+    st.sidebar.markdown("---")  # Separator
+    if st.sidebar.button("Freunde einladen"):
+        show_my_friends()
+    if st.sidebar.button("Einstellungen"):
+        show_settings()
 
 
 def show_my_friends():
     st.write("Meine Freunde")
 
+
 def show_settings():
     st.write("Einstellungen")
 
-
-def save_data_to_database_login():
-    st.session_state.github.write_df(DATA_FILE, st.session_state.df_login, "Updated registration data")
-
-def save_data_to_database_food():
-    if 'github' in st.session_state:
-        st.session_state.github.write_df(DATA_FILE_FOOD, st.session_state.df_food, "Updated food data")
 
 def main():
     init_github()
     init_dataframe_food()
     show_fresh_alert_page()
+
 
 if __name__ == "__main__":
     main()
